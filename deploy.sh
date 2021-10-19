95828d5413097f67b455b94f504e0079149ae9de
Aaron Beavers <foamy125@gmail.com>
2021-10-18 23:38:32 -0400

    correct some file names, add some comments for deleteing the app

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,18 @@
+
+# kubectl delete -f volume-claims/alerts.yaml
+# kubectl delete -f volume-claims/backups.yml
+# kubectl delete -f volume-claims/logs.yml
+# kubectl delete -f volume-claims/world.yaml
+# kubectl delete -f volume-claims/serverfiles.yaml
+# kubectl delete -f deployment.yaml
+# kubectl delete -f load-balancer.yaml
+
 kubectl apply -f volume-claims/alerts.yaml
-kubectl apply -f volume-claims/backups.yaml
-kubectl apply -f volume-claims/logs.yaml
+kubectl apply -f volume-claims/backups.yml
+kubectl apply -f volume-claims/logs.yml
 kubectl apply -f volume-claims/world.yaml
 kubectl apply -f volume-claims/serverfiles.yaml
-
 kubectl apply -f deployment.yaml
 kubectl apply -f load-balancer.yaml
+
+#kubectl logs -f $( kubectl get pods | grep 7dtd | awk '{print $1}')
